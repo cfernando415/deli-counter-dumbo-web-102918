439f694c0636990e13181f5eb9deabb54534e49a
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-09 22:28:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |customer, line|
       lines << " #{line + 1}. #{customer}"
     end
-    puts "The line is currently:" + lines
+    puts lines
   end
   binding.pry
 end

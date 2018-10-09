2c4872a018458fa101e1ce2f884a7aebf19daad4
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-09 22:22:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.size == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     lines = []
     katz_deli.each_with_index do |customer, line|

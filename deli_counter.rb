42d937cc0f402b07d7c0a60c0940f35b03f28e63
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-09 22:21:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0
+    return "The line is currently empty."
+  else
+    lines = []
+    katz_deli.each_with_index do |customer, line|
+      lines << " #{line + 1}. #{customer}"
+    end
+    return "The line is currently:" + lines
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{katz_deli.last}. You are number #{katz_deli.index(name) + 1} in line."
+end
+
+def now_serving
+  
+end
